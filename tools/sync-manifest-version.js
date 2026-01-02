d2541608f8a68da6be4595b6f2ed5b79ac6f14b0
Victor Borg <868919+borgius@users.noreply.github.com>
2026-01-02 10:21:03 -0500

    Update version to 2.7.6 in package.json; refactor sync script to use ES modules

--- tools/sync-manifest-version.js
+++ tools/sync-manifest-version.js
@@ -1,16 +1,20 @@
-const fs = require('fs');
-const path = require('path');
+import { readFileSync, writeFileSync } from 'node:fs';
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
 
-const packageJsonPath = path.join(__dirname, '../package.json');
-const manifestJsonPath = path.join(__dirname, '../manifest.json');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-const packageJson = require(packageJsonPath);
-const manifestJson = require(manifestJsonPath);
+const packageJsonPath = join(__dirname, '../package.json');
+const manifestJsonPath = join(__dirname, '../manifest.json');
+
+const packageJson = JSON.parse(readFileSync(packageJsonPath, 'utf8'));
+const manifestJson = JSON.parse(readFileSync(manifestJsonPath, 'utf8'));
 
 if (manifestJson.version !== packageJson.version) {
     console.log(`Syncing manifest.json version from ${manifestJson.version} to ${packageJson.version}...`);
     manifestJson.version = packageJson.version;
-    fs.writeFileSync(manifestJsonPath, JSON.stringify(manifestJson, null, '\t') + '\n');
+    writeFileSync(manifestJsonPath, JSON.stringify(manifestJson, null, '\t') + '\n');
     console.log('Updated manifest.json');
 } else {
     console.log('manifest.json version is already up to date.');
